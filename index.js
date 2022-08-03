ad4a72aff0d407c5a155d9f8ed24ab18c01cc4b5
adevmir <ademirvalonqar@gmail.com>
2022-08-03 18:20:46 -0300

    [Requisito 01] - Criação do endpoint GET /talker

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var fs = require("fs/promises");
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -10,6 +11,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar 
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const dados = await fs.readFile('./talker.json', 'utf-8');
+  if (!dados) return res.status(200).json([]);
+  res.status(200).json(JSON.parse(dados));
 });
 
 app.listen(PORT, () => {
