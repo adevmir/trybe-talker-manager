1bf704b407f85c8a807abce9a95c8aca2cbf1975
adevmir <ademirvalonqar@gmail.com>
2022-08-15 18:33:59 -0300

    [Requisito 03] Criação do endPoint POST /login

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs/promises');
+const crypto = require('crypto');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -27,12 +28,13 @@ app.get('/talker/:id', async (req, res) => {
   res.status(200).json(user);
 });
 
-app.get('/login', async (req, res) => {
-  const { id } = req.params;
-  const dados = await fs.readFile('./talker.json', 'utf-8');
-  const user = JSON.parse(dados).find((item) => item.id === Number(id));
-  if (!user) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  res.status(200).json(user);
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if ([email, password].includes(undefined)) {
+    return res.status(401).json({ message: 'missing fields' });
+  }
+  const token = crypto.randomBytes(8).toString('hex');
+  res.status(200).json({ token });
 });
 
 app.listen(PORT, () => {
